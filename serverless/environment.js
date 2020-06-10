c460fd90cafc91cad7bd3b6645bb889830e2aa0b
Spencer Oberstadt <soberstadt@gmail.com>
2020-06-10 13:01:54 -0400

    remove some env vars

--- serverless/environment.js
+++ serverless/environment.js
@@ -6,8 +6,6 @@ module.exports = () => {
   return {
     ENVIRONMENT: process.env.ENVIRONMENT || 'development',
     ROLLBAR_ACCESS_TOKEN: process.env.ROLLBAR_ACCESS_TOKEN || '',
-    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME || '',
-    CLOUDSEARCH_DOMAIN_ARN: process.env.CLOUDSEARCH_DOMAIN_ARN || '',
-    CLOUDSEARCH_DOCUMENT_ENDPOINT: process.env.CLOUDSEARCH_DOCUMENT_ENDPOINT || ''
+    PRIVATE_KEY: process.env.PRIVATE_KEY || ''
   }
 }
