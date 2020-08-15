8c7e941d81381085291150bf54929e0d3c0bb087
Spencer Oberstadt <soberstadt@gmail.com>
2020-08-14 21:44:49 -0400

    add another expectation

--- test/bot.test.js
+++ test/bot.test.js
@@ -4,7 +4,13 @@ const myProbotApp = require('../src/bot.js')
 const { Probot } = require('probot')
 // Requiring our fixtures
 const payload = require('./fixtures/pull_request.labeled')
-const issueCreatedBody = { body: "I see you added the \"On Staging\" label, I'll get this merged to the staging branch!" }
+const issueCreatedBody = {
+  body: "I see you added the \"On Staging\" label, I'll get this merged to the staging branch!"
+}
+const mergeBody = {
+  base: 'staging',
+  head: 'asdf1234'
+}
 const fs = require('fs')
 const path = require('path')
 
@@ -12,65 +18,62 @@ nock.disableNetConnect()
 
 describe('Staging Merger Bot', () => {
   let probot
-  let mockCert
-
-  beforeAll((done) => {
-    fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err, cert) => {
-      if (err) return done(err)
-      mockCert = cert
-      done()
-    })
-  })
 
   beforeEach(() => {
-    probot = new Probot({ id: 123, cert: mockCert })
+    probot = new Probot({ id: 123, githubToken: 'test' })
     // Load our app into probot
     probot.load(myProbotApp)
   })
 
-  test('creates a comment when tag is added', async () => {
-    // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+  describe('on label add', () => {
+    let commentMock
+    let mergeMock
 
-    // Test that a comment is posted
-    const commentMock = nock('https://api.github.com')
-      .post('/repos/soberstadt/test-merge-repo/issues/2/comments', (body) => {
-        expect(body).toMatchObject(issueCreatedBody)
-        return true
-      })
-      .reply(200)
+    beforeEach(() => {
+      nock('https://api.github.com')
+        .post('/app/installations/2/access_tokens')
+        .reply(200, { token: 'test' })
+      commentMock = nock('https://api.github.com')
+        .post('/repos/soberstadt/test-merge-repo/issues/2/comments', (body) => {
+          expect(body).toMatchObject(issueCreatedBody)
+          return true
+        })
+        .reply(200)
 
-    const encodedConfig = Buffer.from('{enabled: true, label-name: On Staging, comment: true}').toString('base64')
-    nock('https://api.github.com')
-      .get('/repos/soberstadt/test-merge-repo/contents/.github/merge-bot.yml')
-      .reply(200, { content: encodedConfig })
+      const configHash = "{ enabled: true, 'label-name': 'On Staging', comment: true }"
+      const encodedConfig = Buffer.from(configHash).toString('base64')
+      nock('https://api.github.com')
+        .get('/repos/soberstadt/test-merge-repo/contents/.github/merge-bot.yml')
+        .reply(200, { content: encodedConfig })
 
-    // allow test to get PR details
-    nock('https://api.github.com')
-      .get('/repos/soberstadt/test-merge-repo/pulls/2')
-      .reply(200, { head: { ref: 'asdf1234' } })
+      // allow test to get PR details
+      nock('https://api.github.com')
+        .get('/repos/soberstadt/test-merge-repo/pulls/2')
+        .reply(200, { head: { ref: 'asdf1234' } })
 
-    // Test that a merge is posted
-    const mergeMock = nock('https://api.github.com')
-      .post('/repos/soberstadt/test-merge-repo/merges')
-      .reply(200)
+      mergeMock = nock('https://api.github.com')
+        .post('/repos/soberstadt/test-merge-repo/merges', (body) => {
+          console.log(body)
+          expect(body).toMatchObject(mergeBody)
+          return true
+        })
+        .reply(200)
+    })
 
-    // Receive a webhook event
-    await probot.receive({ name: 'pull_request', payload })
+    test('creates a comment when tag is added', async () => {
+      await probot.receive({ name: 'pull_request', payload })
 
-    expect(commentMock.isDone())
-    expect(mergeMock.isDone())
+      expect(commentMock.activeMocks()).toStrictEqual([])
+    })
+
+    test('merges branch into staging', async () => {
+      await probot.receive({ name: 'pull_request', payload })
+
+      expect(mergeMock.activeMocks()).toStrictEqual([])
+    })
   })
 
   afterEach(() => {
     nock.cleanAll()
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
